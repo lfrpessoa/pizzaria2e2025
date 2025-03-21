64cbc22a3494dcd595b1c7f5cdb1252d35248a0c
lfrpessoa <rm95216@estudante.fieb.edu.br>
2025-03-21 09:58:45 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import { useState } from "react"
-
-function App (){
-
+import { useState} from "react"
+ 
+function App(){
+ 
   const [nome, setNome] = useState ('Ronaldo')
-
+ 
+ const Formulario = ()=>{
+ return(
+  <div>
+ <input
+      className="nome"
+      onChange={(e)=>{setNome(e.target.value)}}
+      type="text" />
+ 
+      <button
+        className="botao"
+        onClick={()=>{alert(nome)}}
+        >
+          CLIQUE AQUI
+      </button>
+  </div>
+ )
+}
   return (
     <div>
-      <h3>Pizaria 2E</h3>
-
-      <input 
-       className = "nome"
-       onChange={(e)=>{setNome(e.target.value)}}
-      type="text" />
-
-      <button
-      className="botao"
-      onClick={()=>{alert(nome)}}
-      >
-        CLIQUE AQUI 
-      </button>
-
+      <h3>Pizzaria 2E</h3>
+ 
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
     </div>
+ 
   )
 }
-
+ 
 export default App
+ 
+ 
