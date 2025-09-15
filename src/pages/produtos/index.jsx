76aa514e826967838e1dc2b551773a8c0551ecc1
lfrpessoa <rm95216@estudante.fieb.edu.br>
2025-09-15 10:56:23 +0000

    listagem de produtos

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,17 @@
+const Produtos = () => {
+
+
+    return(
+        <>
+         <h3>Listagem de Produtos</h3>
+      <ul> 
+        <li>Pizza de muçarela</li>
+        <li>Pizza de calabresa</li>
+        <li>Pizza de Baiana</li>
+        <li>Pizza de Portuguesa</li>
+      </ul>
+        </>
+    )
+
+}
+export default Produtos
